1f75e68a90f24729b9d9fde75328da04a834c4e6
Sanket Sable <sanketsable738@gmail.com>
2025-02-12 13:12:50 +0530

    Update Day_11.sql

--- Classes/Day_11.sql
+++ Classes/Day_11.sql
@@ -82,3 +82,70 @@ DELIMITER ;
 
 -- Use the Functions
 SELECT divi_numbers (16, 8) AS divi; -- Returns 2
+
+-- This function concatenates a first name and a last name into a full name.
+DELIMITER //
+
+CREATE FUNCTION full_name(first_name VARCHAR(50), last_name VARCHAR(50))
+RETURNS VARCHAR(100)
+DETERMINISTIC
+BEGIN
+	RETURN CONCAT(first_name, '  ' , last_name);
+END //
+
+DELIMITER ;
+
+-- Use the Function
+SELECT full_name('John', 'Doe') AS name; -- Returns 'John Doe'
+
+-- This function calculates the age of a person based on their birthdate.
+DELIMITER //
+
+CREATE FUNCTION calculate_age(birthdate DATE)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN YEAR(CURDATE()) - YEAR (birthdate) - (DATE_FORMAT(CURDATE(), '%m%d')
+    < DATE_FORMAT(birthdate, '%m%d'));
+END //
+
+DELIMITER ;
+
+-- Use the Function
+SELECT calculate_age('2005-05-21') AS age; -- Returns the age based on the current date
+
+-- This functions checks if a number is even or odd
+DELIMITER //
+
+CREATE FUNCTION is_even_or_odd(num INT)
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN 
+	IF num % 2 = 0 THEN
+		RETURN 'EVEN';
+	ELSE
+		RETURN 'ODD';
+	END IF;
+END //
+
+DELIMITER ;
+
+-- Use the Function
+SELECT is_even_or_odd(7) AS result;  -- odd
+SELECT is_even_or_odd(8) AS result; -- even
+
+
+-- This function calculates the discounted price based on the original price and discount percentage.
+DELIMITER //
+
+CREATE FUNCTION discounted_price(original_price DECIMAL (10, 2), discount_percentage DECIMAL (5, 2))
+RETURNS DECIMAL (10, 2)
+DETERMINISTIC
+BEGIN
+	RETURN original_price * (1 - discount_percentage / 100);
+END //
+
+DELIMITER ;
+
+-- Use the Function
+SELECT discounted_price(100, 20) AS final_price; -- Returns 80.00
