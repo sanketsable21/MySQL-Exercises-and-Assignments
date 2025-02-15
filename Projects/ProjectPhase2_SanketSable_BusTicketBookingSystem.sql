497f36dd899f90dbe7dbd4ff16911d6a2b046f34
Sanket Sable <sanketsable738@gmail.com>
2025-02-15 13:37:08 +0530

    Update ProjectPhase2_SanketSable_BusTicketBookingSystem.sql

--- Projects/ProjectPhase2_SanketSable_BusTicketBookingSystem.sql
+++ Projects/ProjectPhase2_SanketSable_BusTicketBookingSystem.sql
@@ -1,10 +1,14 @@
+-- create database
 CREATE DATABASE Bus_Tickets_Booking_System;
 
+-- drop database
 DROP DATABASE Bus_Tickets_Booking_System;
 
+-- use database
 USE Bus_Tickets_Booking_System;
 
 -- Table 01---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Users(
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
 	username VARCHAR(255),
@@ -14,10 +18,14 @@ CREATE TABLE Users(
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Select for show data
 SELECT * FROM Users;
 
+-- Drop table
 DROP TABLE Users;
 
+
+-- Insert values in table
 INSERT INTO Users (username, password, email, phone)
 VALUES
 	('sanketsable2105','sanketsable2105','sanketsable738@gmail.com','8291621013'),
@@ -30,9 +38,15 @@ VALUES
 	('rushibambarde27','rushibambarde27','rushibambarde2710@gmail.com','7447313953'),
 	('samadhangadhe12','samadhangadhe12','samadhangadhe1212@gmail.com','9356832018'),
 	('pavangadekar14','pavangadekar14','pavangadekar1412@gmail.com','7878464338');
+    
+-- Select column from table
+SELECT username, email, phone FROM Users;
+
+
 
 
 -- Table 02---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Buses(
 	bus_id INT AUTO_INCREMENT PRIMARY KEY,
 	bus_number VARCHAR(255),
@@ -43,8 +57,13 @@ CREATE TABLE Buses(
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Select for show data
 SELECT * FROM Buses;
 
+-- Drop table
+DROP TABLE Buses;
+
+-- Insert values in table
 INSERT INTO Buses (bus_number, bus_name, capacity, operator_name)
 VALUES
 	('MH12AB1234','Green Express','50','Green Travels'),
@@ -59,6 +78,7 @@ VALUES
 	('MH19RT9753','Metro Runner','52','Metro Travels');
 
 -- Table 03---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Routes(
 	route_id INT AUTO_INCREMENT PRIMARY KEY,
 	source VARCHAR(255),
@@ -68,8 +88,13 @@ CREATE TABLE Routes(
 	fare DECIMAL(10,2)
 );
 
+-- Select for show data
 SELECT * FROM Routes;
 
+-- Drop table
+DROP TABLE Routes;
+
+-- Insert values in table
 INSERT INTO Routes (source, destination, distance, estimated_time, fare)
 VALUES
 	('Dombivali','Thane','25','00:45:00','50.00'),
@@ -84,6 +109,7 @@ VALUES
 	('Mumbai','Surat','275','06:30:00','800');
 
 -- Table 04---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Trips(
 	trip_id INT PRIMARY KEY AUTO_INCREMENT,
     bus_id INT,
@@ -95,11 +121,14 @@ CREATE TABLE Trips(
     FOREIGN KEY(bus_id) REFERENCES Buses(bus_id),
     FOREIGN KEY(route_id) REFERENCES Routes(route_id)
 );
-    
+
+-- Select for show data
 SELECT * FROM Trips;
 
+-- Drop table
 DROP TABLE Trips;
 
+-- Insert values in table
 INSERT INTO Trips (bus_id, route_id, available_seats)
 VALUES
 	(1, 1, 45),
@@ -114,6 +143,7 @@ VALUES
 	(10, 10, 37);
 
 -- Table 05---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Bookings(
 	booking_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
@@ -125,10 +155,13 @@ CREATE TABLE Bookings(
     FOREIGN KEY(trip_id) REFERENCES Trips(trip_id)
 );
 
+-- Select for show data
 SELECT * FROM Bookings;
 
+-- Drop table
 DROP TABLE Bookings;
 
+-- Insert values in table
 INSERT INTO Bookings (user_id, trip_id, total_fare)
 VALUES
 	(1, 1, 500),
@@ -143,6 +176,7 @@ VALUES
 	(10, 10, 300);
     
 -- Table 06---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
 CREATE TABLE Payments(
 	payment_id INT PRIMARY KEY AUTO_INCREMENT,
     booking_id INT, 
@@ -153,10 +187,13 @@ CREATE TABLE Payments(
 	FOREIGN KEY(booking_id) REFERENCES Bookings(booking_id)
 );
 
+-- Select for show data
 SELECT * FROM Payments;
 
+-- Drop table
 DROP TABLE Payments;
 
+-- Insert values in table
 INSERT INTO Payments (booking_id, payment_amount, payment_method)
 VALUES
 	(1, 500, 'Credit Card'),
@@ -169,3 +206,134 @@ VALUES
 	(8, 950, 'Cash'),
 	(9, 1300, 'Net Banking'),
 	(10, 1000, 'UPI');
+    
+-- Table 07---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
+CREATE TABLE Feedback(
+	feedback_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    trip_id INT,
+    rating INT,
+    comments VARCHAR(255),
+    feedback_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES Users(user_id),
+    FOREIGN KEY(trip_id) REFERENCES Trips(trip_id)
+);
+
+-- Select for show data
+SELECT * FROM Feedback;
+
+-- Drop table
+DROP TABLE Feedback;
+
+-- Insert values in table
+INSERT INTO Feedback( user_id, trip_id, rating, comments)
+VALUES
+	(1, 1, 5, 'Excellent service! Comfortable ride.'),
+	(2, 2, 4, 'Good experience, but bus was 10 mins late.'),
+	(3, 3, 3, 'Average ride. AC was not working.'),
+	(4, 4, 5, 'Very punctual and clean bus.'),
+	(5, 5, 2, 'Driver was rude. Won’t recommend.'),
+	(6, 6, 4, 'Good trip, but could improve seating.'),
+	(7, 7, 1, 'Worst experience ever. Bus was cancelled.'),
+	(8, 8, 5, 'Loved the ride! Very smooth journey.'),
+	(9, 9, 4, 'Comfortable trip, but delayed 15 mins.'),
+	(10, 10, 3, 'Seats were not very comfortable.');
+    
+-- Table 08---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
+CREATE TABLE Bus_Schedules(
+	schedule_id INT PRIMARY KEY AUTO_INCREMENT,
+    bus_id INT,
+    route_id INT,
+    departure_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    arrival_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    schedule_days SET('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'),
+    status ENUM('Active', 'Inactive') DEFAULT 'Active',
+    FOREIGN KEY(bus_id) REFERENCES Buses(bus_id),
+    FOREIGN KEY(route_id) REFERENCES Routes(route_id)
+);
+
+-- Select for show data
+SELECT * FROM Bus_Schedules;
+
+-- Drop table
+DROP TABLE Bus_Schedules;
+
+-- Insert values in table
+INSERT INTO Bus_Schedules( bus_id, route_id, schedule_days)
+VALUES
+	(1, 1, 'Monday,Tuesday,Wednesday,Thursday,Friday'),
+	(2, 2, 'Saturday,Sunday'),
+	(3, 3, 'Monday,Wednesday,Friday'),
+	(4, 4, 'Monday,Tuesday,Wednesday,Thursday'),
+	(5, 5, 'Saturday,Sunday'),
+	(6, 6, 'Monday,Wednesday,Friday'),
+	(7, 7, 'Tuesday,Thursday'),
+	(8, 8, 'Friday,Saturday'),
+	(9, 9, 'Friday'),
+	(10, 10, 'Sunday');
+    
+-- Table 09---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
+CREATE TABLE Cancellations(
+	cancellation_id INT PRIMARY KEY AUTO_INCREMENT,
+    booking_id INT,
+    cancellation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    cancellation_reason VARCHAR(255),
+    refund_amount DECIMAL(10,2),
+    status ENUM('Refunded', 'Pending', 'No Refund') DEFAULT 'Pending'
+);
+
+-- Select for show data
+SELECT * FROM Cancellations;
+
+-- Drop table
+DROP TABLE Cancellations;
+
+-- Insert values in table
+INSERT INTO Cancellations( booking_id, cancellation_reason, refund_amount)
+VALUES
+	(1, 'Personal reasons', 500),
+    (2, 'Bus was delayed too long', 650),
+    (3, 'Changed travel plans', 750),
+    (4, 'Incorrect booking', 0),
+    (5, 'Illness', 1200),
+    (6, 'Bus service canceled by operator', 800),
+    (7, 'Found a cheaper alternative', 0),
+    (8, 'Missed the bus', 0),
+    (9, 'Weather issues', 1300),
+    (10, 'Bus breakdown', 1000);
+
+
+-- Table 10---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Create table
+CREATE TABLE Bus_Stops(
+	stop_id INT PRIMARY KEY AUTO_INCREMENT,
+    route_id INT,
+    stop_name VARCHAR(255),
+    stop_order INT,
+    arrival_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    departure_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(route_id) REFERENCES Routes(route_id)
+);
+
+-- Select for show data
+SELECT * FROM Bus_Stops;
+
+-- Drop table
+DROP TABLE Bus_Stops;
+
+-- Insert values in table
+INSERT INTO Bus_Stops( route_id, stop_name, stop_order)
+VALUES
+(1, 'Dombivli Depot', 1),
+(2, 'Thane Station', 2),
+(3, 'Mulund Checkpost', 3),
+(4, 'Sion Bus Stand', 4),
+(5, 'Dadar TT', 5),
+(6, 'Pune Station', 1),
+(7, 'Chinchwad', 2),
+(8, 'Talegaon', 3),
+(9, 'Lonavala', 4),
+(10, 'Khandala', 5);
