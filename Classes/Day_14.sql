21a0942032af56a0fd2235226301ac2d5f230c11
Sanket Sable <sanketsable738@gmail.com>
2025-02-26 11:53:31 +0530

    Create Day_14.sql

---
+++ Classes/Day_14.sql
@@ -0,0 +1,16 @@
+USE zoom;
+
+-- DCL
+/*
+Data Control Language (DCL)
+Definition:
+DCL is used to control access to data in a database. It includes commands that grant or revoke permissions
+to users or roles.
+
+Key Commands
+GRANT: Gives a user or role permission to perform specific actions on 
+database objects (like tables, views, etc.).
+REVOKE: Removes previously granted permissions from a user or role.
+*/
+
+-- Step 1: Create a sample Emp table
